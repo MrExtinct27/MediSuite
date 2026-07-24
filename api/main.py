0a0b35b0895513b0e6dd26c5c20ca68f57afef90
Yash Mahajan <114292920+MrExtinct27@users.noreply.github.com>
2026-07-23 23:57:03 -0700

    Add new origin to CORS middleware

--- api/main.py
+++ api/main.py
@@ -46,7 +46,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://medisuite-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
